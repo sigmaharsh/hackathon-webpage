f71d76508e52ab187377c208bd438a6188ac9066
sigmaharsh <hgg030244@gmail.com>
2024-03-31 11:39:13 +0530

    modified upload.html deeted upload.js

--- upload.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('fileInput').addEventListener('change', function() {
-    var file = this.files[0];
-    if (file) {
-      alert('Selected file: ' + file.name);
-    }
-  });
-  
